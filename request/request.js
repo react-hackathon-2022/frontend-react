4b2b33844a806867ac6d29048bdd141672029244
scar-rs <106949109+scar-rs@users.noreply.github.com>
2022-10-11 00:58:58 +0530

    Add files via upload

---
+++ request/request.js
@@ -0,0 +1,18 @@
+function fun(){
+    let btnAdd = document.querySelector('button');
+    let table = document.querySelector('table');
+
+    let temp = `
+            <tr>
+                <td contenteditable="true"></td>
+                <td contenteditable="true"></td>
+                <td contenteditable="true"></td>
+                <td contenteditable="true"></td>
+            </tr>`;
+
+    table.innerHTML += temp;
+}
+
+function  addfile(){
+    document.querySelectorAll('')
+}
